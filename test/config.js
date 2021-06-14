a538bc7c9b7575836f950955ae352077cca8cdec
leomartinez2020 <lowenhard@yahoo.com>
2021-06-14 15:43:53 -0500

    Leave one only test

---
+++ test/config.js
@@ -0,0 +1,18 @@
+module.exports = ({
+    local: {
+        baseUrl: 'http://www.uitestingplayground.com/textinput',
+        buttonName: 'Cypress',
+        launchOptions: { headless: false },
+        timeout: 50000,
+    },
+    CI: {
+        launchOptions: { 
+            executablePath: process.env.PUPPETEER_EXEC_PATH,
+            headless: true,
+            args: ['--no-sandbox'],
+        },
+        timeout: 50000,
+    },
+    test: {},
+    prod: {}
+})[process.env.TESTENV || 'local']
--- test/test.ui.js
+++ test/test.ui.js
@@ -1,22 +1,27 @@
 const puppeteer = require('puppeteer');
-const should = require('chai').should();
+//const should = require('chai').should();
 const expect = require('chai').expect;
+const config = require('./config');
 
 describe('Test UI', () => {
     let browser;
     let page;
+
     before(async() => {
-        browser = await puppeteer.launch();
+        browser = await puppeteer.launch(config.launchOptions);
     });
+
     after(async() => {
         await browser.close();
     });
+
     beforeEach(async() => {
         page = await browser.newPage();
-    })
+    });
+
     it('Check button text', async() => {
-        const newButtonName = 'Updated Name';
-        await page.goto('http://www.uitestingplayground.com/textinput');
+        const newButtonName = config.buttonName;
+        await page.goto(config.baseUrl);
         await page.type('#newButtonName', newButtonName, {delay: 100});
         let boton = await page.$('#updatingButton');
         await boton.click();
